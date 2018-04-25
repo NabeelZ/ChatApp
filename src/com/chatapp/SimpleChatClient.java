d790798e47d593c29b7f7ffc58df8920c1fa497c
unknown <nblzafar@com>
2018-04-26 01:32:40 +0530

    Reduced it to single button with predefined hint for both fields

--- src/com/chatapp/SimpleChatClient.java
+++ src/com/chatapp/SimpleChatClient.java
@@ -1,7 +1,10 @@
-package  com.chatapp;
+package com.chatapp;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.awt.event.MouseEvent;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
@@ -24,20 +27,21 @@ import javax.swing.text.Element;
 import javax.swing.text.Position;
 import javax.swing.text.Segment;
 
+
 public class SimpleChatClient {
-	
-public class IncomingReader implements Runnable {
+
+	public class IncomingReader implements Runnable {
 
 		public void run() {
 			String message;
 			try {
-			while((message = reader.readLine())!= null){
-				System.out.println(" Read : " + message );
-				incoming.append(message+"\n");
-				if (((qScroller.getVerticalScrollBar().getAlignmentY())*10)>10){
-					incoming.setSize(incoming.getColumns(), incoming.getHeight()+10);
+				while ((message = reader.readLine()) != null) {
+					System.out.println(" Read : " + message);
+					incoming.append(message + "\n");
+					if (((qScroller.getVerticalScrollBar().getAlignmentY()) * 10) > 10) {
+						incoming.setSize(incoming.getColumns(), incoming.getHeight() + 10);
+					}
 				}
-			}	
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
@@ -45,11 +49,12 @@ public class IncomingReader implements Runnable {
 		}
 
 	}
+
 	public class SendButtonListener implements ActionListener {
 
 		public void actionPerformed(ActionEvent arg0) {
 			try {
-				writer.println( usernamefield.getText() +" :  "+outgoing.getText());
+				writer.println(usernamefield.getText() + " :  " + outgoing.getText());
 				writer.flush();
 				outgoing.setText("");
 			} catch (Exception e) {
@@ -59,25 +64,18 @@ public class IncomingReader implements Runnable {
 		}
 
 	}
+
 	public class SereverListener implements ActionListener {
 
 		public void actionPerformed(ActionEvent arg0) {
 			serverIP = server.getText();
 			setUpNetWorking();
-
+			System.out.println("Username Set as " + usernamefield.getText());
 		}
-
-	}
-	public class Setusername implements ActionListener{
-
-		public void actionPerformed(ActionEvent arg1) {
-			usernamef = usernamefield.getText();
-			System.out.println("Username Set as " + usernamef);
-
-		}
-
 	}
 	
+	
+
 	JTextArea incoming;
 	JTextField outgoing;
 	BufferedReader reader;
@@ -85,77 +83,68 @@ public class IncomingReader implements Runnable {
 	JTextField server;
 	JTextField usernamefield;
 	String serverIP;
-	String usernamef;
 	Socket sock;
 	JScrollPane qScroller;
-	
+
 	public static void main(String[] args) {
 		SimpleChatClient client = new SimpleChatClient();
 		client.go();
-		}
-	
+	}
+
 	public void go() {
 		JFrame frame = new JFrame("Chat Client");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+
 		// Panel for server setup.
 		JPanel serverpanel = new JPanel();
 		usernamefield = new JTextField(10);
 		server = new JTextField(10);
-		JButton serverButton = new JButton("SET SERVER");
-		JButton username = new JButton("SET USERNAME");
+		JButton serverButton = new JButton("SET");
 		serverButton.addActionListener(new SereverListener());
-		username.addActionListener(new Setusername());
-		serverpanel.add(server);   
-		serverpanel.add(serverButton);
+		serverpanel.add(server);
 		serverpanel.add(usernamefield);
-		serverpanel.add(username);
+		serverpanel.add(serverButton);
+		usernamefield.setText("Username");
+		server.setText("Server");
 		
-
-				
 		// Panel for adding all the components.
 		JPanel mainpanel = new JPanel();
-		mainpanel.setLayout(new BoxLayout(mainpanel,BoxLayout.Y_AXIS));
-		
+		mainpanel.setLayout(new BoxLayout(mainpanel, BoxLayout.Y_AXIS));
+
 		// Text area for incoming texts.
-		incoming = new JTextArea(10,50);
+		incoming = new JTextArea(10, 50);
 		incoming.setLineWrap(true);
 		incoming.setWrapStyleWord(true);
 		incoming.setEditable(false);
-		DefaultCaret caret = (DefaultCaret)incoming.getCaret();
+		DefaultCaret caret = (DefaultCaret) incoming.getCaret();
 		caret.setUpdatePolicy(DefaultCaret.ALWAYS_UPDATE);
 
-
-		
-		//Scroller for above text area.
+		// Scroller for above text area.
 		qScroller = new JScrollPane(incoming);
 		qScroller.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
 		qScroller.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
 		qScroller.setVisible(true);
-		
-		
 
-		
-		// Text field  for outgoing texts.
+		// Text field for outgoing texts.
 		outgoing = new JTextField(20);
 		JButton sendButton = new JButton("SEND");
 		sendButton.addActionListener(new SendButtonListener());
-		
-		//Adding all the components to Panel.
+
+		// Adding all the components to Panel.
 		mainpanel.add(serverpanel);
 		mainpanel.add(qScroller);
 		mainpanel.add(outgoing);
 		mainpanel.add(sendButton);
-		
-		//Adding main panel to frame.
+
+		// Adding main panel to frame.
 		frame.getContentPane().add(mainpanel);
-		frame.setSize(600,300);
+		frame.setSize(600, 300);
 		frame.setVisible(true);
 	}
-	
+
 	public void setUpNetWorking() {
 		try {
-			sock = new Socket(serverIP,5000);
+			sock = new Socket(serverIP, 5000);
 			sock.setKeepAlive(true);
 			InputStreamReader streamReader = new InputStreamReader(sock.getInputStream());
 			reader = new BufferedReader(streamReader);
@@ -163,13 +152,11 @@ public class IncomingReader implements Runnable {
 			System.out.println("Networking OK !");
 			Thread readerThread = new Thread(new IncomingReader());
 			readerThread.start();
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 	}
 
 }
-
-
